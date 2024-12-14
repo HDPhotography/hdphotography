e7475b2780b9db02e9577bb2ef3a86acbdd3e54f
DharshanPalani <dharshanpalani.2006@gmail.com>
2024-12-14 23:49:12 +0530

    Added cooldown in carousel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,16 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
 
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), react()],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        css: {
+          additionalData: '@import "swiper/swiper-bundle.min.css";',
+        },
+      },
+    },
+  },
 });
